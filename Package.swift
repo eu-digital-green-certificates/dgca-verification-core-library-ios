c9cfa0f4ad3f18ccdee172b53ceb1596f7899c88
IgorKhomiak <ihorkhomyak@gmail.com>
2022-03-29 17:12:16 +0300

    Added core library

--- Package.swift
+++ Package.swift
@@ -4,22 +4,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "CoreLibrary",
+    name: "DGCCoreLibrary",
     defaultLocalization: "en",
     platforms: [.iOS(.v12), .macOS(.v10_14)],
     
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "CoreLibrary",
-            targets: ["CoreLibrary"]),
+            name: "DGCCoreLibrary",
+            targets: ["DGCCoreLibrary"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/SwiftyJSON/SwiftyJSON", from: "5.0.1"),
         .package(url: "https://github.com/filom/ASN1Decoder", from: "1.7.1"),
-        .package( name: "SwiftCBOR",
-          url: "https://github.com/eu-digital-green-certificates/SwiftCBOR",
+        .package( name: "SwiftCBOR", url: "https://github.com/eu-digital-green-certificates/SwiftCBOR",
           .branch("master")
         )
     ],
@@ -27,12 +26,12 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "CoreLibrary",
+            name: "DGCCoreLibrary",
             dependencies: ["SwiftCBOR", "SwiftyJSON", "ASN1Decoder"],
             path: "Sources",
             resources: [.process("Resources")]),
         .testTarget(
-            name: "CoreLibraryTests",
-            dependencies: ["CoreLibrary"]),
+            name: "DGCCoreLibraryTests",
+            dependencies: ["DGCCoreLibrary"]),
     ]
 )
