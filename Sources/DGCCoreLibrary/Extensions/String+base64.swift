79a511d78d61acf2e0a56287397772d1681af58e
IgorKhomiak <ihorkhomyak@gmail.com>
2022-03-30 13:04:46 +0300

    add public access

--- Sources/DGCCoreLibrary/Extensions/String+base64.swift
+++ Sources/DGCCoreLibrary/Extensions/String+base64.swift
@@ -8,7 +8,7 @@
 #if os(iOS)
 import UIKit
 
-extension String {
+public extension String {
     func convertBase64StringToImage () -> UIImage? {
         guard let imageData = Data.init(base64Encoded: self, options: .init(rawValue: 0)) else { return nil }
         let image = UIImage(data: imageData)
