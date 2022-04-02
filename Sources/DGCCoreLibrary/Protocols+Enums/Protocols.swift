8409f7cfcd388abb854c92155c6a63b1b8b94356
IgorKhomiak <ihorkhomyak@gmail.com>
2022-04-02 22:51:08 +0300

    Changed protocol name

--- Sources/DGCCoreLibrary/Protocols+Enums/Protocols.swift
+++ Sources/DGCCoreLibrary/Protocols+Enums/Protocols.swift
@@ -31,7 +31,7 @@ public protocol CertificateInspection {
     func updateLocallyStoredData(appType: AppType, completion: @escaping DataCompletionHandler)
 }
 
-public protocol CertificateApplication {
+public protocol MultyTypeCertificate {
     var dateOfBirth: String { get }
     
     var firstName: String { get }
