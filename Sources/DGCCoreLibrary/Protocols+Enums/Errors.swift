d5783f07ca6bd68577343203f895b9a81d7cdf18
IgorKhomiak <ihorkhomyak@gmail.com>
2022-04-25 18:05:38 +0300

    Added error file

---
+++ Sources/DGCCoreLibrary/Protocols+Enums/Errors.swift
@@ -0,0 +1,26 @@
+//
+//  File.swift
+//  
+//
+//  Created by Igor Khomiak on 25.04.2022.
+//
+
+import Foundation
+
+public class ParseErrors {
+  var errors: [ParseError] = []
+}
+
+public enum ParseError {
+  case base45
+  case prefix
+  case zlib
+  case cbor
+  case json(error: String)
+  case version(error: String)
+}
+
+public enum CertificateParsingError: Error {
+    case unknown
+    case parsing(errors: [ParseError])
+}
