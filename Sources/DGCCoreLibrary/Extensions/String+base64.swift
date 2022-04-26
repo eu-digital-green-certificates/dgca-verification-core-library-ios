23f7750fe80e46c777ca2bad3a73ea1ac81d9315
IgorKhomiak <ihorkhomyak@gmail.com>
2022-04-26 21:57:08 +0300

    added string extension

--- Sources/DGCCoreLibrary/Extensions/String+base64.swift
+++ Sources/DGCCoreLibrary/Extensions/String+base64.swift
@@ -5,6 +5,7 @@
 //  Created by Igor Khomiak on 30.03.2022.
 //
 
+import Foundation
 #if os(iOS)
 import UIKit
 
@@ -16,3 +17,28 @@ public extension String {
     }
 }
 #endif
+
+extension String {
+    func base64UrlToBase64() -> String {
+        var str = self
+            .replacingOccurrences(of: "-", with: "+")
+            .replacingOccurrences(of: "_", with: "/")
+        if str.count % 4 != 0 {
+            str.append(String(repeating: "=", count: 4 - str.count % 4))
+        }
+        return str
+    }
+    
+    func base64UrlDecoded() -> String? {
+        var str = self
+            .replacingOccurrences(of: "-", with: "+")
+            .replacingOccurrences(of: "_", with: "/")
+        if str.count % 4 != 0 {
+            str.append(String(repeating: "=", count: 4 - str.count % 4))
+        }
+        if let data = Data(base64Encoded: str, options: .ignoreUnknownCharacters) {
+            return String(data: data, encoding: .utf8)
+        }
+        return nil
+    }
+}
