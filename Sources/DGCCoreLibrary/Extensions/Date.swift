2934a0f21f65f19bea541e65e4486c5473cfadff
IgorKhomiak <ihorkhomyak@gmail.com>
2022-05-09 11:00:32 +0300

    Update date formatter

--- Sources/DGCCoreLibrary/Extensions/Date.swift
+++ Sources/DGCCoreLibrary/Extensions/Date.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 extension Date {
-    static func formatter(for locale: String, utcPosix: Bool = true, utc: Bool = false) -> DateFormatter {
+    private static func formatter(for locale: String, utcPosix: Bool = true, utc: Bool = false) -> DateFormatter {
         let dateTimeFormatter = DateFormatter()
         dateTimeFormatter.dateFormat = locale
         dateTimeFormatter.timeZone = TimeZone.current
@@ -21,16 +21,16 @@ extension Date {
         }
         return dateTimeFormatter
     }
-
-    static let isoFormatter = formatter(for: "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
-    static let dateFormatter = formatter(for: "yyyy-MM-dd")
-    static let dateFormatterYM = formatter(for: "yyyy-MM")
-    static let dateFormatterY = formatter(for: "yyyy")
-    static let dateFormatterFull = formatter(for: "yyyy-MM-dd'T'HH:mm:ss")
-    static let dateTimeFormatter = formatter(for: "yyyy-MM-dd HH:mm", utcPosix: false)
-    static let dateFormatterOffset = formatter(for: "yyyy-MM-dd'T'HH:mm:ssZZZZZ")
-    static let dateFormatterFractional = formatter(for: "yyyy-MM-dd'T'HH:mm:ss.SSSXXXXX")
-
+    
+    private static let isoFormatter = formatter(for: "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+    private static let dateFormatter = formatter(for: "yyyy-MM-dd")
+    private static let dateFormatterYM = formatter(for: "yyyy-MM")
+    private static let dateFormatterY = formatter(for: "yyyy")
+    private static let dateFormatterFull = formatter(for: "yyyy-MM-dd'T'HH:mm:ss")
+    private static let dateTimeFormatter = formatter(for: "yyyy-MM-dd HH:mm", utcPosix: false)
+    private static let dateFormatterOffset = formatter(for: "yyyy-MM-dd'T'HH:mm:ssZZZZZ")
+    private static let dateFormatterFractional = formatter(for: "yyyy-MM-dd'T'HH:mm:ss.SSSXXXXX")
+    
     // 2022-03-30T15:23:39Z
     public var isoString: String {
         Date.isoFormatter.string(from: self)
@@ -39,20 +39,20 @@ extension Date {
     public var dateString: String {
         Date.dateFormatter.string(from: self)
     }
-
+    
     public var dateOffsetString: String {
         Date.dateFormatterOffset.string(from: self)
     }
-
+    
     public var dateTimeString: String {
         Date.dateTimeFormatter.string(from: self)
     }
-
+    
     public init?(isoString: String) {
         guard let date = Date.isoFormatter.date(from: isoString) else { return nil }
         self = date
     }
-  
+    
     public init?(dateString: String) {
         if let date = Date.dateFormatter.date(from: dateString) {
             self = date
@@ -70,18 +70,18 @@ extension Date {
             return nil
         }
     }
-
+    
     public init?(rfc3339DateTimeString str: String) {
         var str = str
         let rfc3339DateTimeFormatter = DateFormatter()
-
+        
         let nsrange = NSRange(str.startIndex..<str.endIndex, in: str)
         if (try? NSRegularExpression(pattern: "\\.[0-9]{6}Z?$", options: []).matches(
             in: str, options: [], range: nsrange))?.isEmpty == false {
             str = str.trimmingCharacters(in: ["Z"])
             str = String(str.prefix(str.count - 3)) + "Z"
         }
-
+        
         for fmt in ["yyyy-MM-dd'T'HH:mm:ssZZZZZ", "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ", "yyyy-MM-dd't'HH:mm:ss'z'",
             "yyyy-MM-dd't'HH:mm:ss.SSS'z'"] {
             rfc3339DateTimeFormatter.dateFormat = fmt
@@ -92,7 +92,7 @@ extension Date {
         }
         return nil
     }
-
+    
     public var localDateString: String {
         let formatter = DateFormatter()
         formatter.locale = .current
