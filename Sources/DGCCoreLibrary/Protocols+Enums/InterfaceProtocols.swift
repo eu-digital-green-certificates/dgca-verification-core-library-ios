63612a2dc0adb416d6242902daf2cb78d3b14d99
IgorKhomiak <ihorkhomyak@gmail.com>
2022-05-23 13:19:51 +0300

    Added shared protocol

---
+++ Sources/DGCCoreLibrary/Protocols+Enums/InterfaceProtocols.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by Igor Khomiak on 23.05.2022.
+//
+
+import Foundation
+
+public protocol DismissControllerDelegate: AnyObject {
+    func userDidDissmis(_ controller: AnyObject)
+}
