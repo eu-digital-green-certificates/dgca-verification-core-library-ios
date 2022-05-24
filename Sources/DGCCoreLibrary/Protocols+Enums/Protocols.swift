9fc67aac98daad080495b651e13867d7cf2dc8c4
IgorKhomiak <ihorkhomyak@gmail.com>
2022-05-24 13:13:47 +0300

    Changed protocol name

--- Sources/DGCCoreLibrary/Protocols+Enums/Protocols.swift
+++ Sources/DGCCoreLibrary/Protocols+Enums/Protocols.swift
@@ -27,7 +27,7 @@
 import Foundation
 import SwiftyJSON
 
-public protocol CertificateInspection {
+public protocol CertificateValidating {
     func validateCertificate(_ certificate: CertificationProtocol) -> ValidityState
 }
 
